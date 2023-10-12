51a0bd3b6c739cfee1862e2fe8d67b789da4fed2
Jay <leedsjays@gmail.com>
2023-10-11 20:02:40 -0500

    Update Template.cpp

--- content/Template.cpp
+++ content/Template.cpp
@@ -1,28 +1,23 @@
-//Randomness stuff, ckmin, ckmax are optional--depends on time
 #include "bits/stdc++.h"
 using namespace std;
- 
 typedef long long ll;
-typedef long double ld;
+typedef long double ld; // change to double if appropriate
 typedef pair<int, int> pi;
-typedef pair<ll,ll> pl;
-typedef pair<ld,ld> pd;
- 
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+
 typedef vector<int> vi;
 typedef vector<ld> vd;
 typedef vector<ll> vl;
 typedef vector<pi> vpi;
 typedef vector<pl> vpl;
 
-#define FOR(i, a, b) for (int i=a; i<(b); i++)
-#define F0R(i, a) for (int i=0; i<(a); i++)
-#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
-#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
-#define trav(a,x) for (auto& a : x)
-#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
- 
+#define FOR(i, a, b) for (int i = a; i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FORd(i, a, b) for (int i = (b) - 1; i >= (a); i--)
+#define F0Rd(i, a) for (int i = (a) - 1; i >= 0; i--)
+#define trav(a, x) for (auto &a : x)
 #define sz(x) (int)(x).size()
-#define mp make_pair
 #define pb push_back
 #define f first
 #define s second
@@ -31,19 +26,14 @@ typedef vector<pl> vpl;
 #define all(x) x.begin(), x.end()
 #define ins insert
 
-template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
-template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
- 
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+const char nl = '\n';
 
+void solve() {
 
- 
-int main() {
-    ios_base::sync_with_stdio(0); cin.tie(0);
-
-
-
-	return 0;
 }
 
-
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	solve();
+	return 0;
+}
