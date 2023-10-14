1ca8bdbc4c572731ae6ab2c1bb3f6c7ec000f408
Timothy Mou <timothymou7@gmail.com>
2023-10-14 15:46:50 -0500

    Update KMP to use std::string

--- content/KMP.cpp
+++ content/KMP.cpp
@@ -1,37 +1,14 @@
-int nxt[maxn]; //next array
-/*Core KMP @param nxt: the resulting "next" array*/
-void build_nxt(char * str,int * nxt, int length){
-	int k = 0;
-	nxt[0] = 0;
-	for (int i = 1;i < length;i++){
-		while (k > 0 && str[k] != str[i]){
-			k = nxt[k - 1];
-		}
-		if (str[k] == str[i]){
-			k++;
-		}
-		nxt[i] = k;
-	}
+vector<int> prefix_function(string s) {
+    int n = sz(s);
+    vector<int> pi(n);
+    for (int i = 1; i < n; i++) {
+        int j = pi[i-1];
+        while (j > 0 && s[i] != s[j])
+            j = pi[j-1];
+        if (s[i] == s[j])
+            j++;
+        pi[i] = j;
+    }
+    return pi;
 }
-/*Matching the string with the pattern
-@return number of occurences of pattern string in the original string
-*/
-int match(char * str, char * pattern, int length_str, int length_pattern){
-	int total = 0;
-	int p = 0;
-	for (int i = 0;i < length_str;i++){
-		while (p > 0 && pattern[p] != str[i]){
-			p = nxt[p - 1];
-		}
-		if (pattern[p] == str[i]){
-			p++;
-		}
-		if (p == length_pattern){
-			total++;
-			p = nxt[p - 1];
-		}
-	}
-	return total;
-}
-//build nxt for pattern
-build_nxt(pattern, nxt, len_pattern);
+
