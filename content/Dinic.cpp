a13d03dc71a7f6414a975686671bf1006d212d9f
Jay <leedsjays@gmail.com>
2023-10-14 18:06:54 -0500

    Update Dinic.cpp

--- content/Dinic.cpp
+++ content/Dinic.cpp
@@ -1,27 +1,27 @@
 //from https://cp-algorithms.com/graph/dinic.html
 //Complexity: O(E*V^2)
-struct FlowEdge {
+struct Edge {
    int v, u;
-   long long cap, flow = 0;
-   FlowEdge(int v, int u, long long cap) : v(v), u(u), cap(cap) {}
+   ll cap, flow = 0;
+   Edge(int v, int u, ll cap) : v(v), u(u), cap(cap) {}
 };
 
 struct Dinic {
-   const long long flow_inf = 1e18;
-   vector<FlowEdge> edges;
-   vector<vector<int>> adj;
+   const ll flow_inf = 1e18;
+   vector<Edge> edges;
+   vector<vi> adj;
    int n, m = 0;
    int s, t;
-   vector<int> level, ptr;
+   vi lev, ptr;
    queue<int> q;
 
    Dinic(int n, int s, int t) : n(n), s(s), t(t) {
        adj.resize(n);
-       level.resize(n);
+       lev.resize(n);
        ptr.resize(n);
    }
 
-   void add_edge(int v, int u, long long cap) {
+   void add_edge(int v, int u, ll cap) {
        edges.emplace_back(v, u, cap);
        edges.emplace_back(u, v, 0);
        adj[v].push_back(m);
@@ -33,29 +33,29 @@ struct Dinic {
        while (!q.empty()) {
            int v = q.front();
            q.pop();
-           for (int id : adj[v]) {
+           trav(id, adj[v]) {
                if (edges[id].cap - edges[id].flow < 1)
                    continue;
-               if (level[edges[id].u] != -1)
+               if (lev[edges[id].u] != -1)
                    continue;
-               level[edges[id].u] = level[v] + 1;
+               lev[edges[id].u] = lev[v] + 1;
                q.push(edges[id].u);
            }
        }
-       return level[t] != -1;
+       return lev[t] != -1;
    }
 
-   long long dfs(int v, long long pushed) {
-       if (pushed == 0)
+   ll dfs(int v, ll pu) {
+       if (pu == 0)
            return 0;
        if (v == t)
-           return pushed;
-       for (int& cid = ptr[v]; cid < (int)adj[v].size(); cid++) {
+           return pu;
+       for (int& cid = ptr[v]; cid < sz(adj[v]); cid++) {
            int id = adj[v][cid];
            int u = edges[id].u;
-           if (level[v] + 1 != level[u] || edges[id].cap - edges[id].flow < 1)
+           if (lev[v] + 1 != lev[u] || edges[id].cap - edges[id].flow < 1)
                continue;
-           long long tr = dfs(u, min(pushed, edges[id].cap - edges[id].flow));
+           ll tr = dfs(u, min(pu, edges[id].cap - edges[id].flow));
            if (tr == 0)
                continue;
            edges[id].flow += tr;
@@ -65,17 +65,17 @@ struct Dinic {
        return 0;
    }
 
-   long long flow() {
-       long long f = 0;
+   ll flow() {
+       ll f = 0;
        while (true) {
-           fill(level.begin(), level.end(), -1);
-           level[s] = 0;
+           fill(all(lev), -1);
+           lev[s] = 0;
            q.push(s);
            if (!bfs())
                break;
-           fill(ptr.begin(), ptr.end(), 0);
-           while (long long pushed = dfs(s, flow_inf)) {
-               f += pushed;
+           fill(all(ptr), 0);
+           while (ll pu = dfs(s, flow_inf)) {
+               f += pu;
            }
        }
        return f;
