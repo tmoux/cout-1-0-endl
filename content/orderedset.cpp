64471d9f1b9bb2413badd44b54b21f035009d5c9
nitrousoxide95 <wangrz0306@gmail.com>
2024-09-07 20:29:54 +0800

    Keep up with KACTL Data Structures
    Keep up with KACTL Data Structures

--- content/orderedset.cpp
+++ content/orderedset.cpp
@@ -1,7 +1,20 @@
-#include <ext/pb_ds/assoc_container.hpp>
+/* Description: A set (not multiset!) with support for finding the n'th
+ * element, and finding the index of an element.
+ * To get a map, change \texttt{null\_type}. */
+
+#include <bits/extc++.h> /** keep-include */
 using namespace __gnu_pbds;
 
-template<typename T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-/* order_of_key(k), how many elements < k */
-/* find_by_order(k), k'th element (from 0) */
+template<class T>
+using Tree = tree<T, null_type, less<T>, rb_tree_tag,
+    tree_order_statistics_node_update>;
+
+void example() {
+	Tree<int> t, t2; t.insert(8);
+	auto it = t.insert(10).first;
+	assert(it == t.lower_bound(9));
+	assert(t.order_of_key(10) == 1);
+	assert(t.order_of_key(11) == 2);
+	assert(*t.find_by_order(0) == 8);
+	t.join(t2); // assuming T < T2 or T > T2, merge t2 into t
+}
